f5decd8ac79102bff50b5592c4294af04f7cc3f4
unknown <rahul>
2023-05-09 10:45:58 +0530

    first commit

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,10 +1,10 @@
 import RahulPatil from '../assets/pdf/RahulPatil.pdf'
-import profileImg from '../assets/png/profileImg.jpg'
+import profileImg from '../assets/png/p1.jpg'
 
 export const headerData = {
     name: 'Rahul Patil',
     title: "Full Stack Software Engineer",
-    desciption:"A developer with strong passion for self-improvement. I have strong problem solving skills & I bring energy, patience, positivity and cultural values to the team.",
+    desciption: "I'm a full-stack engineer who loves AI - I'm always trying to teach machines to think like humans, so I can finally take a break. When I'm not coding, you'll find me explaining to my friends why robots are going to take over the world (they're not convinced yet).",
     image: profileImg,
     resumePdf: RahulPatil
 }
--- src/data/socialsData.js
+++ src/data/socialsData.js
@@ -1,10 +1,10 @@
 export const socialsData = {
     github: 'https://github.com/rahul3355',
     facebook: 'https://www.facebook.com/',
-    linkedIn: 'https://www.linkedin.com/in/rahulpatil35/',
+    linkedIn: 'https://www.linkedin.com/in/rahulcss/',
     instagram: 'https://www.instagram.com/',
     codepen: 'https://codepen.io/',
-    twitter: 'https://twitter.com/rahul33553',
+    twitter: 'https://twitter.com/rahulcss3',
     reddit: 'https://www.reddit.com/user/',
     blogger: 'https://www.blogger.com/',
     medium: 'https://medium.com/@',
